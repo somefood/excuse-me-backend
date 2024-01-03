50e205d8305f61d4460fce51ffea3d28433ed5e7
seokhwan-an <asok369258@naver.com>
2024-01-03 16:52:23 +0900

    #1 feat : spring boot 프로젝트 생성

---
+++ src/main/java/choorai/excuseme/ExcuseMeApplication.java
@@ -0,0 +1,13 @@
+package choorai.excuseme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcuseMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcuseMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/choorai/excuseme/ExcuseMeApplicationTests.java
@@ -0,0 +1,13 @@
+package choorai.excuseme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcuseMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
