aa344abe908aa4e31a779f54b75a919cadd7947b
seokhwan-an <asok369258@naver.com>
2024-01-06 20:21:32 +0900

    #11 refactor: 파라미터에 final 추가 및 불필요한 공백제거

--- src/test/java/choorai/excuseme/global/exception/TestErrorCode.java
+++ src/test/java/choorai/excuseme/global/exception/TestErrorCode.java
@@ -2,7 +2,6 @@ package choorai.excuseme.global.exception;
 
 import org.springframework.http.HttpStatus;
 
-
 public enum TestErrorCode implements ErrorCode{
 
     TEST_ERROR(HttpStatus.BAD_REQUEST, "잘못된 정보 입력", "해당 입력은 잘못되었습니다.");
@@ -11,7 +10,7 @@ public enum TestErrorCode implements ErrorCode{
     private final String errorCode;
     private final String errorMessage;
 
-    TestErrorCode(HttpStatus httpStatus, String errorCode, String errorMessage) {
+    TestErrorCode(final HttpStatus httpStatus, final String errorCode, final String errorMessage) {
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
