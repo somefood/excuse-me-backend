c817d2851ac2b27c3cf379367810c328956adc9e
Seokju Hong <iamseokjuhong@gmail.com>
2024-01-07 00:07:08 +0900

    #5 feat: spring security 설정
    - 의존성 추가
    - formLogin, CSRF, httpBasic disable
    - SessionCreationPolicy stateless

---
+++ src/main/java/choorai/excuseme/global/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package choorai.excuseme.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(configurer -> configurer.disable());
+        http.csrf(configurer -> configurer.disable());
+        http.formLogin(configurer -> configurer.disable());
+        http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
