f8cf041c722f465d89fb021b5976ffb266904d14
seokhwan-an <asok369258@naver.com>
2024-02-19 16:22:04 +0900

    #7: member 객체 추가 필드 구성

--- src/main/java/choorai/excuseme/member/domain/Member.java
+++ src/main/java/choorai/excuseme/member/domain/Member.java
@@ -1,7 +1,19 @@
 package choorai.excuseme.member.domain;
 
 import choorai.excuseme.global.domain.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,33 +22,69 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(indexes = {
-        @Index(name = "idx_username", columnList = "username", unique = true)
+    @Index(name = "idx_username", columnList = "username", unique = true)
 })
 public class Member extends BaseEntity {
+
+    private static final String DATE_FORMAT = "yyyyMMdd";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
     private String username;
 
     private String password;
 
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    private Member(String username, String password, Role role) {
+    private Member(final String username,
+                   final String password,
+                   final String name,
+                   final String gender,
+                   final String birthDate,
+                   final String phoneNUmber,
+                   final Role role
+    ) {
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.gender = Gender.valueOf(gender);
+        this.birthDate = LocalDate.parse(birthDate, DateTimeFormatter.ofPattern(DATE_FORMAT));
+        this.phoneNumber = phoneNUmber;
         this.role = role;
     }
 
-    public static Member createNormalMember(String username, String password) {
-        return new Member(username, password, Role.USER);
+    public static Member createNormalMember(final String username,
+                                            final String password,
+                                            final String name,
+                                            final String gender,
+                                            final String birthDate,
+                                            final String phoneNumber
+    ) {
+        return new Member(username, password, name, gender, birthDate, phoneNumber, Role.USER);
     }
 
-    public static Member createAdminMember(String username, String password) {
-        return new Member(username, password, Role.ADMIN);
+    public static Member createAdminMember(final String username,
+                                           final String password,
+                                           final String name,
+                                           final String gender,
+                                           final String birthDate,
+                                           final String phoneNUmber
+    ) {
+        return new Member(username, password, name, gender, birthDate, phoneNUmber, Role.ADMIN);
     }
 }
