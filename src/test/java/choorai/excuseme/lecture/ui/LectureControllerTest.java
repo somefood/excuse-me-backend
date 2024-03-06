4ccdf5a8109a3fd1179bdb59e5b38df0e0330931
Seokju Hong <iamseokjuhong@gmail.com>
2024-03-07 00:54:37 +0900

    #31: 전체 강의 조회 테스트 작성
    강의 시청 테스트에서 멤버 생성방식 수정

--- src/test/java/choorai/excuseme/lecture/ui/LectureControllerTest.java
+++ src/test/java/choorai/excuseme/lecture/ui/LectureControllerTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import choorai.excuseme.global.security.JwtProvider;
 import choorai.excuseme.lecture.domain.Lecture;
+import choorai.excuseme.lecture.domain.dto.LectureDetailResponse;
 import choorai.excuseme.lecture.domain.dto.LectureRequest;
+import choorai.excuseme.lecture.domain.dto.LectureResponse;
 import choorai.excuseme.lecture.domain.repository.LectureRepository;
 import choorai.excuseme.member.domain.Member;
 import choorai.excuseme.member.domain.Role;
@@ -15,7 +17,11 @@ import choorai.excuseme.memberlecutre.domain.repository.MemberLectureRepository;
 import choorai.excuseme.support.AcceptanceTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -48,13 +54,45 @@ class LectureControllerTest extends AcceptanceTest {
     }
 
     @Test
+    @DisplayName("전체 강의 목록을 조회한다.")
+    void find_all_lectures() throws Exception {
+        // given
+        final Member normalMember = Member.createNormalMember(
+            "a@email.com",
+            passwordEncoder.encode("password1@"),
+            "이름",
+            "MEN",
+            "19990101",
+            "01012345678"
+        );
+        memberRepository.save(normalMember);
+
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            lectureRepository.save(new Lecture("lecture" + i, "thumbnail" + i, "videoUrl" + i));
+        });
+
+        final String accessToken = jwtProvider.createToken("a@email.com", Role.USER);
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured
+            .given().headers("Authorization", "Bearer " + accessToken)
+            .when().get("/lectures")
+            .then().statusCode(HttpStatus.OK.value())
+            .extract();
+
+        // then
+        final List<LectureResponse> lectureResponses = response.jsonPath().getList("", LectureResponse.class);
+        assertThat(lectureResponses.size()).isEqualTo(10);
+    }
+
+    @Test
     @DisplayName("강의를 시청한다.")
     void watch_lecture() throws Exception {
         // given
         final Member normalMember = Member.createNormalMember(
-            "excuseme",
-            passwordEncoder.encode("excuseme"),
-            "excuseme",
+            "a@email.com",
+            passwordEncoder.encode("password1@"),
+            "이름",
             "MEN",
             "19990101",
             "01012345678"
@@ -64,7 +102,7 @@ class LectureControllerTest extends AcceptanceTest {
         final Lecture lecture = new Lecture("lecture1", "thumbnail", "videoUrl");
         lectureRepository.save(lecture);
 
-        final String accessToken = jwtProvider.createToken("excuseme", Role.USER);
+        final String accessToken = jwtProvider.createToken("a@email.com", Role.USER);
         final LectureRequest lectureRequest = new LectureRequest(ProgressStatus.COMPLETED);
 
         // when
