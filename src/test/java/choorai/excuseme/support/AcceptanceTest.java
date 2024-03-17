b73503e0e3d21f348448f591d55afd2ffd3a2bf4
somefood <iamseokjuhong@gmail.com>
2024-03-17 16:38:38 +0900

    #41: testcontainers 테스트

--- src/test/java/choorai/excuseme/support/AcceptanceTest.java
+++ src/test/java/choorai/excuseme/support/AcceptanceTest.java
@@ -2,17 +2,51 @@ package choorai.excuseme.support;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AcceptanceTest {
+public abstract class AcceptanceTest {
+
+    private static final String ROOT = "root";
+    private static final String ROOT_PASSWORD = "test";
+
+    @Autowired
+    private DataInitializer dataInitializer;
+    @Container
+    protected static MySQLContainer container;
+
+    static {
+        container = (MySQLContainer) new MySQLContainer("mysql:8.0")
+                .withDatabaseName("excuse-me")
+                .withEnv("MYSQL_ROOT_PASSWORD", ROOT_PASSWORD);
+
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> ROOT);
+        registry.add("spring.datasource.password", () -> ROOT_PASSWORD);
+        registry.add("spring.flyway.url", container::getJdbcUrl);
+        registry.add("spring.flyway.user", () -> ROOT);
+        registry.add("spring.flyway.password", () -> ROOT_PASSWORD);
+    }
 
     @LocalServerPort
     private int port;
 
     @BeforeEach
-    void setUp() {
+    void delete() {
+        dataInitializer.deleteAll();
         RestAssured.port = port;
     }
 }
---
+++ src/test/java/choorai/excuseme/support/DataInitializer.java
@@ -0,0 +1,58 @@
+package choorai.excuseme.support;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Profile("test")
+public class DataInitializer {
+
+    private static final int OFF = 0;
+    private static final int ON = 1;
+    private static final String FLYWAY = "flyway";
+    private static final String TRUNCATE = "TRUNCATE ";
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final List<String> truncationDMLs = new ArrayList<>();
+
+    @BeforeEach
+    @Transactional
+    public void deleteAll() {
+        if (truncationDMLs.isEmpty()) {
+            init();
+        }
+
+        setForeignKeyEnabled(OFF);
+        truncateAllTables();
+        setForeignKeyEnabled(ON);
+    }
+
+    private void setForeignKeyEnabled(final int enabled) {
+        em.createNativeQuery("SET foreign_key_checks = " + enabled).executeUpdate();
+    }
+
+    private void truncateAllTables() {
+        truncationDMLs.stream()
+                .map(em::createNativeQuery)
+                .forEach(Query::executeUpdate);
+    }
+
+    private void init() {
+        final List<String> tableNames = em.createNativeQuery("SHOW TABLES ").getResultList();
+
+        tableNames.stream()
+                .filter(name -> !name.contains(FLYWAY))
+                .map(TRUNCATE::concat)
+                .forEach(truncationDMLs::add);
+    }
+}
